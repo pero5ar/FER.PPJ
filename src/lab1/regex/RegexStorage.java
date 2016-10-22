6cd6731cf1d73194fa756ec5a1f8fd6742afb82e
Juraj Juričić <juraj.juricic@gmail.com>
2016-10-22 18:28:49 +0200

    Add RegexStoragebasic implementation

--- src/lab1/regex/RegexStorage.java
+++ src/lab1/regex/RegexStorage.java
@@ -1,7 +1,35 @@
 package lab1.regex;
 
-public class RegexStorage {
-	public void addDefinition(String name, String definition){
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
+public class RegexStorage  {
+	private HashMap<String, Set<String>> storage;
+
+	public RegexStorage(){
+		this.storage = new HashMap<>();
+	}
+
+	public void addDefinition(String name, String definition){
+		storage.put(name, parseDefinition(definition));
+	}
+
+	private Set<String> parseDefinition(String definition){
+		String[] parts = definition.split("\\|");
+
+		Set<String> set = new HashSet<>();
+		for(String elem : parts){
+			set.add(elem);
+		}
+
+		return set;
+	}
+
+	public static void main(String[] args){
+		String test = "{oktalnaZnamenka}|8|9";
+
+		RegexStorage rs = new RegexStorage();
+		rs.addDefinition("oktalnaZnamenka", test);
 	}
 }
