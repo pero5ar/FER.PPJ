fbcb59c60671b62234090a22c489f7206834f4ff
pero5ar <pero.5ar@gmail.com>
2016-10-23 02:26:09 +0200

    Add RegEx prototype

---
+++ src/lab1/regex/RegEx.java
@@ -0,0 +1,17 @@
+package lab1.regex;
+
+
+/**
+ * Created by pero5ar on 23.10.2016..
+ */
+public class RegEx {
+    private String definition;
+
+    public RegEx(String definition) {
+        this.definition = definition;
+    }
+
+    public boolean startsWith(String str) {
+        return false;
+    }
+}
